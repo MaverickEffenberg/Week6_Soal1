99fe88321ea90fa81a9febd4cfbb76fb7d6360fb
MaverickEffenberg <meffenberg@student.ciputra.ac.id>
2025-10-18 13:48:57 +0700

    2

--- app/src/main/java/com/maverick/week6_soal1/MainActivity.kt
+++ app/src/main/java/com/maverick/week6_soal1/MainActivity.kt
@@ -3,45 +3,11 @@ package com.maverick.week6_soal1
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.maverick.week6_soal1.ui.theme.Week6Theme
+import com.maverick.week6_soal1.ui.views.PandaApp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            Week6Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
-        }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Week6Theme {
-        Greeting("Android")
+        setContent { PandaApp() }
     }
 }
